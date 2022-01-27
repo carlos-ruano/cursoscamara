0d5b994dee4b0371a743bcb63e46ef74f1b0228b
pablobd90 <pablobd90@gmail.com>
2022-01-27 12:17:02 +0100

    actualización página edicción de cursos

--- views/editCourses_view.php
+++ views/editCourses_view.php
@@ -6,8 +6,8 @@
         <div class="courseEditBox">
             <div class="image"><img src="<?= Config::PATH_IMG . "fotoprueba.jpg"?>" alt=""></div>
             <div class="name"><h4><?= $course->getName() ?? '' ?></h4></div>
-            <div class="delete"></div>
-            <div class="edit"></div>
+            <div class="delete">Borrar</div>
+            <div class="edit">Editar</div>
         </div>
 
     <?php } ?>
