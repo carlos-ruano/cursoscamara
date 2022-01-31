3ecff91d72bc1f32ffe89663cbbcac5359501e92
pablobd90 <pablobd90@gmail.com>
2022-01-31 12:51:25 +0100

    actualización página de información

--- views/course_info.php
+++ views/course_info.php
@@ -1,13 +1,26 @@
-<div>
-    <div class="courseBox">
-        <div class="nombre"><?= $this->course->getName() ?? '' ?></div>
+<div class="infoCurso">
+    <div class="nombre"><?= $this->course->getName() ?? '' ?></div>
+    <div class="infoPrincipal">
         <div class="imagen"><img src="<?= Config::PATH_IMG.'fotoprueba.jpg'?>" alt=""></div>
-        <div class="dato"> Fecha de inicio: <?= $this->course->getStart_date() ?? '' ?></div>
-        <div class="dato"> Fecha de finalización: <?= $this->course->getEnd_date() ?? '' ?></div>
-        <div class="dato"> Duración: <?= $this->course->getDuration() ?? '' ?> horas</div>
-        <div class="contacto">
-            <a href="tel:+34616923002" class="llamanos">Llámanos</a>
-            <a href="<?= $this->course->getWeb_link() ?? '' ?>" class="inscribete">Inscríbete</a>
+        <div class="infoBasica">
+            <h3>Datos del curso</h3>
+            <p class="datoIzq"><div class="dato"> Fecha de inicio:</p> <p class="datoDer"><?= $this->course->getStart_date() ?? '' ?></p></div>
+            <p class="datoIzq"><div class="dato"> Fecha de finalización:</p> <p class="datoDer"><?= $this->course->getEnd_date() ?? '' ?></p></div>
+            <p class="datoIzq"><div class="dato"> Duración:</p> <p class="datoDer"><?= $this->course->getDuration() ?? '' ?> horas</p></div>
+            <p class="datoIzq"><div class="dato"> Lugar:</p> <p class="datoDer"><?= $this->course->getPlace() ?? '' ?> </p></div>
+            <p class="datoIzq"><div class="dato"> Horario:</p> <p class="datoDer"><?= $this->course->getSchedule() ?? '' ?> </p></div>
+            <h3>Contacto</h3>
+            <p class="datoIzq"><div class="dato"> Teléfono:</p> <p class="datoDer"><?= $this->course->getContact_telephone() ?? '' ?> </p></div>
+            <p class="datoIzq"><div class="dato"> E-mail:</p> <p class="datoDer"><?= $this->course->getContact_email() ?? '' ?> </p></div>
         </div>
+    </div>
+    <div class="description">
+        <h3>Descripción</h3>
+        <div><p><?= $this->course->getDescription() ?? ''?></p></div>
+    </div>
+    <div class="contacto">
+        <a href="tel:+34616923002" class="botonContacto">Llámanos</a>
+        <a href="" class="botonContacto">PDF</a>
+        <a href="<?= $this->course->getWeb_link() ?? '' ?>" class="botonContacto">Inscríbete</a>
     </div>
 </div>
