a70b33f52ff819c3f2106126cbea212bd32dc5dc
carlos-ruano <cdr.ruano@gmail.com>
2022-01-31 09:26:12 +0100

    nose

--- controllers/CoursesController.php
+++ controllers/CoursesController.php
@@ -14,10 +14,10 @@ class CoursesController {
         // } else {
         //     $view->classEnlaces = 'ocultar';
         // }
-        $courses=$this->model->getCourses();
+        $courses = $this->model->getCourses();
         $view->courses = $courses;
         $view->urlReturn = Config::URL_BASE;
-        $view->urlEditCourses = Config::URL_BASE."courses/editCourses";
+        $view->urlEditCourses = Config::URL_BASE . "courses/editCourses";
         $view->render('courses');
     }
 
@@ -32,8 +32,8 @@ class CoursesController {
                 if ($validate->isOk()) {
                     $course = new Course($_POST);
                     $id = $this->model->createCourse($course);
-                    if($id!==false){
-                        header("location:".Config::URL_BASE."courses/editCourses");
+                    if ($id !== false) {
+                        header("location:" . Config::URL_BASE . "courses/editCourses");
                     }
                     $_POST = [];
                 }
@@ -47,13 +47,13 @@ class CoursesController {
         }
     }
 
-    function editCourses(){
+    function editCourses() {
 
         /*if (!($_SESSION["autenticado"] && $_SESSION["rol"] === 'administrador')) {
             header("location:" . Config::URL_BASE);
         }*/
         $view = new View();
-        $courses=$this->model->getCourses();
+        $courses = $this->model->getCourses();
         $view->courses = $courses;
         $view->urlReturn = Config::URL_BASE;
         $view->url_newCourse = Config::URL_BASE . "/courses/new";
@@ -77,23 +77,27 @@ class CoursesController {
                 header("location:" . Config::URL_BASE . "courses/editCourses");
             }
             if (isset($_POST["enviar"])) {
+                //var_dump($_POST);
+                //var_dump($_FILES);
                 $validate = new CourseValidator($_POST);
                 if ($validate->isOk()) {
                     $course = new Course($_POST);
                     $course->setId($id);
-                    $course->setImage_link($courseOld->getImagen());
-                    if ($_FILES["image"]["name"] !== "") {
+                    $course->setImage_link($courseOld->getImage_link());
+                    var_dump($course);    
+                    
+                    /*if ($_FILES["image"]["name"] !== "") {
                         $tmp = $_FILES["image"]["tmp_name"];
                         $name = $_FILES["image"]["name"];
                         $size = $_FILES["image"]["size"];
                         $type = $_FILES["image"]["type"];
-                        $path=new ConfigPath();
+                        $path = new ConfigPath();
                         $destino = $path->path_imagenes . "/" . $name;
                         if ($type === "image/jpeg") {
                             $ok = move_uploaded_file($tmp, $destino);
                             if ($ok) {
                                 $course->setImage_link($name);
-                                $file = $path->path_imagenes . "/" . $courseOld->getImagen();
+                                $file = $path->path_imagenes . "/" . $courseOld->getImage();
                                 if (file_exists($file)) {
                                     unlink($file);
                                 }
@@ -104,11 +108,12 @@ class CoursesController {
                         }
                     } else {
                         $view->imagen = Config::URL_IMG . "/" . $courseOld->getImagen();
-                    }
-
+                    }*/
+                    
                     $id = $this->model->editCourse($course);
-                    if ($id!==false){
-                        header("location:".Config::URL_BASE."courses/editCourses");
+                    if ($id !== false) {
+                        header("location:" . Config::URL_BASE . "courses/editCourses");
+                        echo "Se ha actualizado el curso";
                     }
                 }
             } else {
@@ -139,12 +144,12 @@ class CoursesController {
         }
     }
 
-    function info(int $id){
+    function info(int $id) {
         $view = new View();
-        $course=$this->model->getCourse($id);
+        $course = $this->model->getCourse($id);
         $view->course = $course;
         $view->urlReturn = Config::URL_BASE;
-        $view->urlCourseInfo = Config::URL_BASE."courses/info/".$id;
+        $view->urlCourseInfo = Config::URL_BASE . "courses/info/" . $id;
         $view->render('info');
     }
 
@@ -184,9 +189,4 @@ class CoursesController {
             echo "<br><a href='" . Config::URL_BASE . "courses/editCourses'>Volver</a>";
         }
     }
-
-
-
-
-
 }
