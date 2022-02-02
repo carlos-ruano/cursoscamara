b127af597c0ccb9a27ab7eacd1c6af23203f6c4e
carlos-ruano <cdr.ruano@gmail.com>
2022-02-02 09:49:35 +0100

    configuracion config y configdev

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
     require_once "./libs/Autoload.php";
     Autoload::init();
-    require_once "./config/Config.php";
+    require_once "./config/Config-dev.php";
+    require_once "./config/ConfigDB.php";
     session_start();
     require "./views/header.php";
-    var_dump(substr($_SERVER["REQUEST_URI"],14));
-    echo "<br>";
-    var_dump($_GET["url"]);
-    if(isset($_GET["url"])){
-        $url = $_GET["url"];
+    $url = substr($_SERVER["REQUEST_URI"],14);
+    if($url != ""){
         $router = new Router($url);
     } else {
         $ctrl = new HomeController();
