29aeedb5328df7493f5a69c30078842dbaf0a604
pablobd90 <pablobd90@gmail.com>
2022-02-03 13:51:12 +0100

    actualización mensaje de error 3

--- views/signup_view.php
+++ views/signup_view.php
@@ -7,28 +7,28 @@
             <div class="tipoDato"><label for="name">Nombre: </label></div>
             <div class="divDato">
                 <div class="dato"><input type="text" id="name" name="name" value="<?= $_POST['name'] ?? "" ?>"></div>
-                <div><?= $this->errores['name'] ?? "" ?></div>
+                <div class="error"><?= $this->errores['name'] ?? "" ?></div>
             </div>
         </div>
         <div class="celda">
             <div class="tipoDato"><label for="email">Email: </label></div>
             <div class="divDato">
                 <div class="dato"><input type="email" id="email" name="email" value="<?= $_POST['email'] ?? "" ?>"></div>
-                <div><?= $this->errores['email'] ?? "" ?></div>
+                <div class="error"><?= $this->errores['email'] ?? "" ?></div>
             </div>
         </div>
         <div class="celda">
             <div class="tipoDato"><label for="password">Contraseña: </label></div>
             <div class="divDato">
                 <div class="dato"><input type="password" id="password" name="password"></div>
-                <div><?= $this->errores['password'] ?? "" ?></div>
+                <div class="error"><?= $this->errores['password'] ?? "" ?></div>
             </div>
         </div>
         <div class="celda">
             <div class="tipoDato"><label for="pass2">Repita la contraseña: </label></div>
             <div class="divDato">
                 <div class="dato"><input type="password" id="pass2" name="pass2"></div>
-                <div><?= $this->errores['pass2'] ?? "" ?></div>
+                <div class="error"><?= $this->errores['pass2'] ?? "" ?></div>
             </div>
         </div>
         <br>
