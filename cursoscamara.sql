5d9394ce9ef760ad5bd0adf49a752b91ee3e36dd
carlos-ruano <cdr.ruano@gmail.com>
2022-02-04 08:35:07 +0100

    cambio bbdd

--- cursoscamara.sql
+++ cursoscamara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-02-2022 a las 09:05:55
+-- Tiempo de generación: 04-02-2022 a las 08:34:01
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -35,12 +35,12 @@ CREATE TABLE `courses` (
   `duration` int(4) DEFAULT NULL,
   `place` varchar(255) DEFAULT NULL,
   `schedule` varchar(255) DEFAULT NULL,
-  `contact_email` varchar(255) DEFAULT NULL,
-  `contact_telephone` varchar(255) DEFAULT NULL,
+  `contact_email` varchar(255) DEFAULT 'camaratoledo@camaratoledo.com',
+  `contact_telephone` varchar(255) DEFAULT '925822561',
   `description` text DEFAULT NULL,
-  `web_link` varchar(255) DEFAULT NULL,
-  `pdf_link` varchar(255) DEFAULT NULL,
-  `image_link` varchar(255) DEFAULT NULL,
+  `web_link` varchar(255) DEFAULT 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/',
+  `pdf_link` varchar(255) DEFAULT '	https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/	',
+  `image_link` varchar(255) DEFAULT 'default_img.jpg',
   `status` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -49,12 +49,13 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `name`, `start_date`, `end_date`, `duration`, `place`, `schedule`, `contact_email`, `contact_telephone`, `description`, `web_link`, `pdf_link`, `image_link`, `status`) VALUES
-(1, 'CURSO DE VIGILANTE DE SEGURIDAD', '2021-12-01', '2021-12-30', NULL, 'Cámara de Comercio de Toledo', 'De lunes a viernes, de 9:00 a 15:00 horas', '', '', 'El objetivo de este curso es preparar vigilantes de seguridad para la obtención de la certificación acreditativa correspondiente, expedida en un centro de formación de personal de seguridad privada homologado por el Ministerio del Interior.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/vigilante-de-seguridad-en-toledo/', 'https://camaratoledo.com/wp-content/uploads/2021/11/vigilante-seguridad-toledo.pdf', 'vigilante-seguridad.jpg', NULL),
-(2, 'CURSO DE CAMARERA/O DE PISOS', NULL, NULL, 215, 'Cámara de Comercio de Toledo', 'De lunes a viernes, de 8:00 a 14:30 horas', NULL, '925822561', 'El objetivo de este curso es preparar vigilantes de seguridad para la obtención de la certificación acreditativa correspondiente, expedida en un centro de formación de personal de seguridad privada homologado por el Ministerio del Interior.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/curso-de-camarera-o-de-pisos-en-talavera/', 'https://camaratoledo.com/wp-content/uploads/2021/03/camarera-de-pisos-talavera-2021.pdf', 'auxiliar-camarero.jpg', 'proximamente'),
-(3, 'CURSO DE OPERACIONES BÁSICAS CON FIBRA ÓPTICA E INSTALACIÓN DE PLACAS SOLARES ', NULL, NULL, 215, 'Cámara de Comercio, Plaza del Pan, 11. Talavera de la Reina.', 'De lunes a viernes, de 9 a 14 horas', NULL, '925822561', 'A jóvenes mayores de 16 y menores de 29 años beneficiarios del Sistema de Garantía Juvenil interesados en el sector de las placas solares.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/operaciones-basicas-con-fibra-optica-e-instalacion-de-placas-solares/', NULL, 'fibra-optica.jpg', 'proximamente'),
-(18, 'wcsdc', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', 'pexels-karolina-grabowska-5206922.jpg', NULL),
-(20, 'asas', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', 'pexels-marlene-leppänen-2188596.jpg', NULL),
-(21, 'eewsew', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', 'DSC_0166.JPG', NULL);
+(1, 'CURSO DE VIGILANTE DE SEGURIDAD', '2021-12-01', '2021-12-30', NULL, 'Cámara de Comercio de Toledo', 'De lunes a viernes, de 9:00 a 15:00 horas', '', '', 'El objetivo de este curso es preparar vigilantes de seguridad para la obtención de la certificación acreditativa correspondiente, expedida en un centro de formación de personal de seguridad privada homologado por el Ministerio del Interior.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/vigilante-de-seguridad-en-toledo/', 'https://camaratoledo.com/wp-content/uploads/2021/11/vigilante-seguridad-toledo.pdf', 'vigilante-seguridad.jpg', 'disponible'),
+(2, 'CURSO DE CAMARERA/O DE PISOS', NULL, NULL, 215, 'Cámara de Comercio de Toledo', 'De lunes a viernes, de 8:00 a 14:30 horas', NULL, '925822561', 'El objetivo de este curso es preparar vigilantes de seguridad para la obtención de la certificación acreditativa correspondiente, expedida en un centro de formación de personal de seguridad privada homologado por el Ministerio del Interior.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/curso-de-camarera-o-de-pisos-en-talavera/', 'https://camaratoledo.com/wp-content/uploads/2021/03/camarera-de-pisos-talavera-2021.pdf', 'auxiliar-camarero.jpg', 'disponible'),
+(3, 'CURSO DE OPERACIONES BÁSICAS CON FIBRA ÓPTICA E INSTALACIÓN DE PLACAS SOLARES ', NULL, NULL, 215, 'Cámara de Comercio, Plaza del Pan, 11. Talavera de la Reina.', 'De lunes a viernes, de 9 a 14 horas', NULL, '925822561', 'A jóvenes mayores de 16 y menores de 29 años beneficiarios del Sistema de Garantía Juvenil interesados en el sector de las placas solares.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/operaciones-basicas-con-fibra-optica-e-instalacion-de-placas-solares/', NULL, 'fibra-optica.jpg', 'disponible'),
+(18, 'aaaaaaaaaaa', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', 'default_img.jpg', NULL),
+(20, 'asas', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', 'default_img.jpg', NULL),
+(21, 'eewsew', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', 'default_img.jpg', NULL),
+(25, 'sds', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -102,7 +103,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
