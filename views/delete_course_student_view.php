4bfb5a98e4be94fcae19c37fd39d9c90b80bbbe9
pablobd90 <pablobd90@gmail.com>
2022-02-09 11:16:38 +0100

    actualización

--- views/delete_course_student_view.php
+++ views/delete_course_student_view.php
@@ -2,7 +2,7 @@
     <br>
         <a href="<?=$this->urlReturn?>" class="cancel">Volver</a>
         <a href="<?=$this->url_delete_only_from_course?>" class="botonBorrar">Eliminar de curso</a>
-        <a href="<?=$this->url_delete_complete?>" class="botonBorrar">Eliminar alumno</a>
+        <a href="<?=$this->url_delete_complete?>" class="botonBorrar">Eliminar alumno por completo</a>
     <br>
     <?php include "student_info.php"; ?>
 </div>
