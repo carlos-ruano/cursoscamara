cad10fb4b0b88f81dadbdf55f96822fd2b7aa0d0
carlos-ruano <cdr.ruano@gmail.com>
2022-02-10 09:02:49 +0100

    cambios hoy

--- cursoscamara.sql
+++ cursoscamara.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-02-2022 a las 08:00:02
+-- Tiempo de generación: 10-02-2022 a las 09:01:41
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -51,7 +51,8 @@ CREATE TABLE `courses` (
 INSERT INTO `courses` (`id`, `name`, `start_date`, `end_date`, `duration`, `place`, `schedule`, `contact_email`, `contact_telephone`, `description`, `web_link`, `pdf_link`, `image_link`, `status`) VALUES
 (1, 'CURSO DE VIGILANTE DE SEGURIDAD', '2021-12-01', '2021-12-30', NULL, 'Cámara de Comercio de Toledo', 'De lunes a viernes, de 9:00 a 15:00 horas', '', '', 'El objetivo de este curso es preparar vigilantes de seguridad para la obtención de la certificación acreditativa correspondiente, expedida en un centro de formación de personal de seguridad privada homologado por el Ministerio del Interior.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/vigilante-de-seguridad-en-toledo/', 'https://camaratoledo.com/wp-content/uploads/2021/11/vigilante-seguridad-toledo.pdf', 'vigilante-seguridad.jpg', 'disponible'),
 (2, 'CURSO DE CAMARERA/O DE PISOS', NULL, NULL, 215, 'Cámara de Comercio de Toledo', 'De lunes a viernes, de 8:00 a 14:30 horas', NULL, '925822561', 'El objetivo de este curso es preparar vigilantes de seguridad para la obtención de la certificación acreditativa correspondiente, expedida en un centro de formación de personal de seguridad privada homologado por el Ministerio del Interior.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/curso-de-camarera-o-de-pisos-en-talavera/', 'https://camaratoledo.com/wp-content/uploads/2021/03/camarera-de-pisos-talavera-2021.pdf', 'auxiliar-camarero.jpg', 'disponible'),
-(3, 'CURSO DE OPERACIONES BÁSICAS CON FIBRA ÓPTICA E INSTALACIÓN DE PLACAS SOLARES ', NULL, NULL, 215, 'Cámara de Comercio, Plaza del Pan, 11. Talavera de la Reina.', 'De lunes a viernes, de 9 a 14 horas', NULL, '925822561', 'A jóvenes mayores de 16 y menores de 29 años beneficiarios del Sistema de Garantía Juvenil interesados en el sector de las placas solares.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/operaciones-basicas-con-fibra-optica-e-instalacion-de-placas-solares/', NULL, 'fibra-optica.jpg', 'disponible');
+(3, 'CURSO DE OPERACIONES BÁSICAS CON FIBRA ÓPTICA E INSTALACIÓN DE PLACAS SOLARES ', NULL, NULL, 215, 'Cámara de Comercio, Plaza del Pan, 11. Talavera de la Reina.', 'De lunes a viernes, de 9 a 14 horas', NULL, '925822561', 'A jóvenes mayores de 16 y menores de 29 años beneficiarios del Sistema de Garantía Juvenil interesados en el sector de las placas solares.', 'https://camaratoledo.com/programa-integral-de-cualificacion-y-empleo-pice/cursos-de-formacion-pice/operaciones-basicas-con-fibra-optica-e-instalacion-de-placas-solares/', NULL, 'fibra-optica.jpg', 'disponible'),
+(40, 'prueba2', '0000-00-00', '0000-00-00', NULL, '', '', '', '', '', '', '', '966314.jpg', NULL);
 
 -- --------------------------------------------------------
 
@@ -77,13 +78,10 @@ CREATE TABLE `students` (
 
 INSERT INTO `students` (`id`, `name`, `surname`, `dni`, `telephone`, `garantia`, `pice`, `orientation`, `observations`) VALUES
 (1, 'Carlos', 'Dominguez', '11111111N', '664106030', '2022-02-05', '2022-02-05', '2022-02-06', 'Todo bien'),
-(2, 'holasdff', 'asss', '11111111N', 'a', '2022-02-05', '2022-02-05', '2022-02-06', 'asa'),
-(5, 'carlitos', 'sadd', 'wewew', 'wewewe', '2022-02-10', '2022-02-23', '2022-02-26', 'wewewew'),
-(6, 'ads', 'sad', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
-(7, 'wdeew', 'weddew', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
-(8, 'dadasd', 'sadasda', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
-(9, 'adsad', 'asdsd', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
-(10, 'hola', 'holis', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '');
+(30, 'hola', 'holis', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
+(31, 'p1', 'p1', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
+(34, 'www', 'ww', '', '', '0000-00-00', '0000-00-00', '0000-00-00', ''),
+(35, 'prueb10', 'p10', '', '', '0000-00-00', '0000-00-00', '0000-00-00', '');
 
 -- --------------------------------------------------------
 
@@ -102,11 +100,11 @@ CREATE TABLE `students_courses` (
 
 INSERT INTO `students_courses` (`student_id`, `course_id`) VALUES
 (1, 1),
-(2, 1),
-(7, 1),
-(8, 1),
-(9, 1),
-(10, 3);
+(30, 1),
+(30, 2),
+(30, 3),
+(34, 2),
+(35, 40);
 
 -- --------------------------------------------------------
 
@@ -167,13 +165,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de la tabla `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
