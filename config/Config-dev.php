c5c55cabc8f7862f31a9e7819818cefe11bf6e4f
carlos-ruano <cdr.ruano@gmail.com>
2022-02-10 10:05:27 +0100

    cambios hostin

--- config/Config-dev.php
+++ config/Config-dev.php
@@ -2,11 +2,10 @@
 
 class Config {
     const URL_BASE = 'http://localhost/cursoscamara/';
-    const PATH_BASE = "C:/xampp/htdocs/cursoscamara/";
+    //const PATH_BASE = "C:/xampp/htdocs/cursoscamara/";
     const PATH_CSS = Config::URL_BASE . "content/styles/style.css";
     const PATH_IMG = Config::URL_BASE . "content/img/";
     const PATH_LOGIN = Config::URL_BASE . "login";
-    const PATH_IMAGES = Config::PATH_BASE . "content/img/";
     const NAME_CHARACTER_MAX = 50;
     const EMAIL_CHARACTER_MAX = 100;
     const PASS_CHARACTER_MAX = 255;
